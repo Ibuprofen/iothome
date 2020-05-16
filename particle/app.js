be1c91a3cbaef088fd4a0b49e3cf5a55b607ceb8
Tony Rieker <tony@plenary.org>
2020-05-15 23:45:03 -0700

    indentation, auto gen stuff

--- particle/app.js
+++ particle/app.js
@@ -9,37 +9,37 @@ process.on('SIGINT', () => {
 })
 
 particle.login({
-	username: process.env.PARTICLE_USERNAME, password: process.env.PARTICLE_PASSWORD
+  username: process.env.PARTICLE_USERNAME, password: process.env.PARTICLE_PASSWORD
 }).then((result) => {
-	console.log('Your access token', result.body.access_token)
+  console.log('Your access token', result.body.access_token)
 
-	const token = result.body.access_token
+  const token = result.body.access_token
 
-	particle.getEventStream({
-		deviceId: 'mine',
-		auth: token
-	}).then((stream) => {
-		stream.on('end', () => {
-			console.log('end')
-		})
-		stream.on('event', (res) => {
-			console.log('event')
-			if (res.data) {
-				console.log(res)
+  particle.getEventStream({
+    deviceId: 'mine',
+    auth: token
+  }).then((stream) => {
+    stream.on('end', () => {
+      console.log('end')
+    })
+    stream.on('event', (res) => {
+      console.log('event')
+      if (res.data) {
+        console.log(res)
 
-				// TODO: change shape of incoming object
-				if (res.name && res.name === 'dsTmp') {
-					logEvent(res)
-				}
-			}
-		})
-		stream.on('error', (err) => {
-			console.error(err)
-		})
-	}, (err) => {
-		console.error(err)
-	})
+        // TODO: change shape of incoming object
+        if (res.name && res.name === 'dsTmp') {
+          logEvent(res)
+        }
+      }
+    })
+    stream.on('error', (err) => {
+      console.error(err)
+    })
+  }, (err) => {
+    console.error(err)
+  })
 
 }, (err) => {
-	console.error(err)
+  console.error(err)
 })
--- particle/utils.js
+++ particle/utils.js
@@ -5,28 +5,29 @@ const http = require('http')
 
 function logEvent(obj) {
 
-    const unixTimeNs = `${new Date(obj.published_at).getTime()}000000`
-    const data = `weather,deviceid=${obj.coreid} tempf=${obj.data} ${unixTimeNs}`
+  const unixTimeNs = `${new Date(obj.published_at).getTime()}000000`
+  const data = `weather,deviceid=${obj.coreid} tempf=${obj.data} ${unixTimeNs}`
 
-    const req = http.request({
-        hostname: 'influxdb',
-        port: 8086,
-        path: '/write?db=main',
-        method: 'POST',
-    }, (res) => {
-        res.setEncoding('utf8')
-        res.on('data', (body) => {
-            console.log(body)
-        })
-    })  
-
-    req.on('error', (e) => {
-      console.log(`problem with request: ${e}`)
+  const req = http.request({
+    hostname: 'influxdb',
+    port: 8086,
+    path: '/write?db=main',
+    method: 'POST',
+  }, (res) => {
+    res.setEncoding('utf8')
+    res.on('data', (body) => {
+      console.log(body)
     })
-    // write data to request body
-    console.log('writing', data)
-    req.write(data)
-    req.end()
+  })
+
+  req.on('error', (e) => {
+    console.log(`problem with request: ${e}`)
+  })
+
+  // write data to request body
+  console.log('writing', data)
+  req.write(data)
+  req.end()
 }
 
 module.exports = { logEvent }
