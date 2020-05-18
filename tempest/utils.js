8029e9b44d23ddbe678908f221c1f5dfa019c3cd
Tony Rieker <tony@plenary.org>
2020-05-18 14:03:13 -0700

    more decimal places for rain totals

--- tempest/utils.js
+++ tempest/utils.js
@@ -5,12 +5,12 @@ const http = require('http')
 
 const isNum = (n) => typeof n === "number";
 const parseFixed = (n, digits = 2) => Number.parseFloat(n).toFixed(digits);
-const calc = (n) => isNum(n) ? parseFixed(n) : NaN;
+const calc = (n, digits = 2) => isNum(n) ? parseFixed(n, digits) : NaN;
 
 const cToF = celcius => calc(celcius * 1.8 + 32);
 const mbarToHg = mbar => calc(mbar / 33.8639);
 const kmToMiles = km => calc(km * 0.62137);
-const mmToIn = mm => calc(mm / 25.4);
+const mmToIn = mm => calc(mm / 25.4, 10);
 
 function logEvent(measurement, tags, fields, unix_time) {
   const unixTimeNs = `${new Date(unix_time * 1000).getTime()}000000`
@@ -22,26 +22,26 @@ function logEvent(measurement, tags, fields, unix_time) {
   // weather,deviceid=001 tempf=50 <largenumber>
   const line = `${measurement},${tagStr} ${fieldStr} ${unixTimeNs}`
 
-  const req = http.request({
-    hostname: 'influxdb',
-    port: 8086,
-    path: '/write?db=main',
-    method: 'POST',
-  }, (res) => {
-    res.setEncoding('utf8')
-    res.on('data', (body) => {
-      console.log(body)
-    })
-  })
+  // const req = http.request({
+  //   hostname: 'influxdb',
+  //   port: 8086,
+  //   path: '/write?db=main',
+  //   method: 'POST',
+  // }, (res) => {
+  //   res.setEncoding('utf8')
+  //   res.on('data', (body) => {
+  //     console.log(body)
+  //   })
+  // })
 
-  req.on('error', (e) => {
-    console.log(`problem with request: ${e}`)
-  })
+  // req.on('error', (e) => {
+  //   console.log(`problem with request: ${e}`)
+  // })
 
-  // write data to request body
+  // // write data to request body
   console.log('writing', line)
-  req.write(line)
-  req.end()
+  // req.write(line)
+  // req.end()
 }
 
 module.exports = { logEvent, cToF, mbarToHg, kmToMiles, mmToIn }
